f05c8371b1c68aadfe98a50ee6f450cf95c8e0f0
Vladyslav Avrashov <vladisloveavrashov@gmail.com>
2025-02-06 14:00:50 +0200

    try fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function getElementWidth(content, padding, border) {
     return Number.parseFloat(content) + Number.parseFloat(padding) + Number.parseFloat(border) + Number.parseFloat(padding) + Number.parseFloat(border);
 }
